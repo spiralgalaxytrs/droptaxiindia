c93d6ccc22cf05f1761983f9d587047882a24f6d
spiralgalaxytrs <80592602+spiralgalaxytrs@users.noreply.github.com>
2021-07-06 22:24:48 +0530

    Update map.js

--- js/map.js
+++ js/map.js
@@ -45,16 +45,37 @@ function calcRoute() {
             //Get distance and time            
             var d=result.routes[0].legs[0].distance.value/1000;
             var n = d.toFixed(0);
-            sedano=((n*13)+400)+" - "+((n*13)+550);
-            sedanr=2*((n*11)+400)+" - "+2*((n*11)+550);
-            suvo=((n*18)+400)+" - "+((n*18)+550);
-            suvr=2*((n*13)+350)+" - "+2*((n*13)+500);
-            traveller=(2*((n*18)+350))+" - "+(2*((n*18)+500));
+            sedano=((n*13)+550);
+            sedanr=2*((n*11)+550);
+            psedano=((n*14)+550);
+            psedanr=2*((n*12)+550);
+            suvo=((n*18)+550);
+            suvr=2*((n*14)+550);
+            psuvo=((n*20)+550);
+            psuvr=2*((n*16)+550);
+            traveller=(2*((n*18)+550));
             
-            
-            
-         $("#output1").html('<h4 class="pt-classic-title ab-center">Sedan - Swift, Zest</h4><h4 class="pt-classic-price-value ab-center">Distance - '+n+'</h4><div class="pt-classic-divider"></div><ul class="list-objects-inline"><li><span class="icon text-primary mdi mdi-seat-recline-extra"></span><span class="imp-bo">4 seater</span></li> <li><span class="icon text-primary mdi mdi-air-conditioner"></span><span class="imp-bo">Available</span></li></ul><h4 class="pt-classic-title ab-center">Extra Charges</h4><ul class="pt-classic-list"><li>Extra KM - 14/km</li><li>Toll and State </li><li>Parking & Hill station </li></ul><div class="pt-classic-price-outer ab-center"><div class="pt-classic-price" aria-hidden="false"><div class="pt-classic-price-currency">₹ </div><div class="pt-classic-price-value">'+sedano+'</div></div><div class="pt-classic-price" aria-hidden="true"><div class="pt-classic-price-currency">$</div><div class="pt-classic-price-value">470</div></div></div>');
-            
+                var strText8 = document.getElementById("ser").value;
+                var oneway = document.getElementById("oneway");
+                var twoway = document.getElementById("twoway");
+            if(strText8=="One-Way"){
+                oneway.style.display = "block";
+                     $(".Dist").html(n);
+                     $("#1").html(psedano);
+                     $("#3").html(psuvo);    
+                     $("#1").html(sedano);     
+                     $("#7").html(suvo);    
+            }
+            else{
+                twoway.style.display = "block";
+                $(".Dist").html(2*n);
+                $("#2").html(psedanr);
+                $("#4").html(psuvr);
+                $("#6").html(suvr);
+                $("#2").html(sedanr);
+                $("#9").html(traveller);
+            }
+        
            
           //  $("#output").html("<div class='result-table'> Driving distance: " + result.routes[0].legs[0].distance.text + ".<br />SUV Rate: ₹" +((n)*12)+"-"+((n)*15)+".<br />Sedan Rate: ₹" +((n)*8)+"-"+((n)*11)+".<br />Hatchback Rate: ₹" +((n)*16)+"-"+((n)*19)+ ".<br />Duration: " + result.routes[0].legs[0].duration.text + ".</div>");
             //document.getElementById("output").style.display = "block";
