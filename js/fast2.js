394ef8f020129156e4b0c2b764e06f5c4e3c377a
sourav dhar <18cs113@gmail.com>
2021-07-09 21:21:25 +0530

    #12

--- js/fast2.js
+++ js/fast2.js
@@ -45,7 +45,7 @@ function book() {
     var result;
     if(strText8=="One-Way"){
         console.log("result set in one way");
-        result = 'Customer Name:  ' + strText + '%0APhone Number: ' + strText1 +'%0APickup Location: ' + strText2+  '%0ADrop Location: ' + strText3 + '%0APickup Date: ' + strText4 + '%0APickup Time: ' + strText6 +'%0AService: ' + strText8 + '%0ACars: ' + strText7+"%0APrice:"+rate;
+        result = 'Customer Name:  ' + strText + '%0APhone Number: ' + strText1 +'%0APickup Location: ' + strText2+  '%0ADrop Location: ' + strText3 + '%0APickup Date: ' + strText4 + '%0APickup Time: ' + strText6 +'%0AService: ' + strText8 + '%0ACars: ' + strText7+"%0APrice:"+rate+"%0ATotalKms: "+n;
     }else{
         result = 'Customer Name:  ' + strText + '%0APhone Number: ' + strText1 +'%0APickup Location: ' + strText2+  '%0ADrop Location: ' + strText3 + '%0APickup Date: ' + strText4 +'%0ADrop Date:'+ strText9 + '%0APickup Time: ' + strText6 +'%0AService: ' + strText8 + '%0ACars: ' + strText7+"%0APrice:"+rate;
     }
@@ -62,7 +62,6 @@ if(strText==""||strText1==""||strText2==""||strText3==""||strText4==""||strText6
      request.open("post", url);
     request.send();
     console.log("Sent Telegram successfully");
-    var n = document.getElementById("nElement").value;
     fetch("https://www.fast2sms.com/dev/bulkV2?authorization=tXGFnAr4LNYZM8Q9jwPVHxWdvs6eahl2qk5of7SzpRbOUEumITTnoiOFHqdCLhzJPgaxer2mpZ8UNEyf&route=dlt&sender_id=SMSTRS&message=126247&variables_values=%20" + strText + "%20%7C%20" + Clink + "%20%7C%20" + CID + "%20%7C%20" + strText + "%20%7C%20" + strText1 + "%20%20%7C%20" + strText2 + "%20%7C%20" + strText3 + "%20%20%7C%20" + strText4 + "%20%20%20" + strText6 + "%20%20%7C%20" + strText7 + "%20%20%20%20" + strText8 + "%20%20%7C%20" + n + "%20%7C" + rate + "%7C" + 15 + "%7C" + 450 + "%7C%20" + CPh + "%20" + "%20%7C&flash=0&numbers=" + strText1)
     .then(response=>{
            
