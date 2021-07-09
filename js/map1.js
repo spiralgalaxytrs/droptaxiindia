ceaae2945fcd18843bebd2ee82ab1c6f91adc467
spiralgalaxytrs <80592602+spiralgalaxytrs@users.noreply.github.com>
2021-07-09 23:02:30 +0530

    Update map1.js

--- js/map1.js
+++ js/map1.js
@@ -1,84 +1,151 @@
+var myLatLng = { lat: 13.0827, lng: 80.2707 };
+var mapOptions = {
+    center: myLatLng,
+    zoom: 1,
+    mapTypeId: google.maps.MapTypeId.ROADMAP
+};
+
+// Hide result box
+//document.getElementById("output").style.display = "none";
+//document.getElementById("output1").style.display = "block";
+// Create/Init map
+var map = new google.maps.Map(document.getElementById('google-map'), mapOptions);
+
+// Create a DirectionsService object to use the route method and get a result for our request
+var directionsService = new google.maps.DirectionsService();
+
+// Create a DirectionsRenderer object which we will use to display the route
+var directionsDisplay = new google.maps.DirectionsRenderer();
+
+// Bind the DirectionsRenderer to the map
+directionsDisplay.setMap(map);
 var sedano;
 var sedanr;
 var suvo; 
 var suvr;
+var n;
 var psuvo;
 var psuvr;
 var traveller;
 
-function book() {
-    var CID =Math.floor((Math.random()*1000000)+1);
-    var strText = document.getElementById("name").value;
-    var strText1 = document.getElementById("p").value;
-    var strText2 = document.getElementById("location-1").value;
-    var strText3 = document.getElementById("location-2").value;
-    var strText4 = document.getElementById("pickupdate").value;
-    var strText9 = document.getElementById("dropdate").value;
-    var strText6 = document.getElementById("pickuptime").value;
-    var strText7 = document.getElementById("cars").value;
-    var strText8 = document.getElementById("ser").value;
-   
-    var Clink="https://droptaxiindia.com";
-    var CPh="99409 80956";
-    var Cname="Drop Taxi India";
-    //calcRoute()
-    if((strText7=="Sedan") && (strText8=="One-Way"))
-    {rate=sedano;}
-    else if((strText7=="Sedan") && (strText8="Round"))
-    {rate=sedanr;}
-    else if((strText7=="SUV") && (strText8=="One-Way"))
-    {rate=suvo;}
-    else if((strText7=="SUV") && (strText8=="Round"))
-    {rate=suvr;}
-    else if((strText7=="PSedan") && (strText8=="One-Way"))
-    {rate=psedano;}
-    else if((strText7=="PSedan") && (strText8="Round"))
-    {rate=psedanr;}
-    else if((strText7=="PSUV") && (strText8=="One-Way"))
-    {rate=psuvo;}
-    else if((strText7=="PSUV") && (strText8=="Round"))
-    {rate=psuvr;}
-    else if(strText7=="Traveller")
-    {rate=traveller;}
-    console.log("read successful");
-    console.log("book",strText8);
-    var result;
-    var carTypemsg = strText7[0]=='P'?strText7.replace('P',"Prime "):strText7;
-    if(strText8=="One-Way"){
-        console.log("result set in one way");
-        result = 'Customer Name:  ' + strText + '%0APhone Number: ' + strText1 +'%0APickup Location: ' + strText2+  '%0ADrop Location: ' + strText3 + '%0APickup Date: ' + strText4 + '%0APickup Time: ' + strText6 +'%0AService: ' + strText8 + '%0ACars: ' + carTypemsg+"%0APrice:"+rate+"%0ATotalKms: "+n;
-    }else{
-        result = 'Customer Name:  ' + strText + '%0APhone Number: ' + strText1 +'%0APickup Location: ' + strText2+  '%0ADrop Location: ' + strText3 + '%0APickup Date: ' + strText4 +'%0ADrop Date:'+ strText9 + '%0APickup Time: ' + strText6 +'%0AService: ' + strText8 + '%0ACars: ' + strText7+"%0APrice:"+rate+"%0ATotalKms: "+n;
+// Define calcRoute function
+function calcRoute() {
+     var y = document.getElementById("ma");
+        y.style.display = "block";
+        var z = document.getElementById("Location");
+        z.style.display = "none";
+    //create request
+    var request = {
+        origin: document.getElementById("location-1").value,
+        destination: document.getElementById("location-2").value,
+        travelMode: google.maps.TravelMode.DRIVING,
+        unitSystem: google.maps.UnitSystem.METRIC
     }
-    console.log(result);
-    document.getElementById("bookingForm").addEventListener("submit", (e) => {
-e.preventDefault();
-if(strText==""||strText1==""||strText2==""||strText3==""||strText4==""||strText6=="")
-        {
-        console.log('error');
-        }else
-        {
-            const request = new XMLHttpRequest();
-        const url = 'https://api.telegram.org/bot1721938726:AAEqc3VUsUo88GIeAcrWvhRhFVvpmU1aJSE/sendMessage?chat_id=-518646833&text=' + result;
-     request.open("post", url);
-    request.send();
-    console.log("Sent Telegram successfully");
-    fetch("https://www.fast2sms.com/dev/bulkV2?authorization=tXGFnAr4LNYZM8Q9jwPVHxWdvs6eahl2qk5of7SzpRbOUEumITTnoiOFHqdCLhzJPgaxer2mpZ8UNEyf&route=dlt&sender_id=SMSTRS&message=126247&variables_values=%20" + strText + "%20%7C%20" + Clink + "%20%7C%20" + CID + "%20%7C%20" + strText + "%20%7C%20" + strText1 + "%20%20%7C%20" + strText2 + "%20%7C%20" + strText3 + "%20%20%7C%20" + strText4 + "%20%20%20" + strText6 + "%20%20%7C%20" + strText7 + "%20%20%20%20" + strText8 + "%20%20%7C%20" + n + "%20%7C" + rate + "%7C" + 15 + "%7C" + 450 + "%7C%20" + CPh + "%20" + "%20%7C&flash=0&numbers=" + strText1)
-    .then(response=>{
-           
-            if(response.status==200)
-            {
-                console.log("Sent Message successfully");
-                localStorage.setItem("result", strText);
-                localStorage.setItem("result1", strText2);
-                localStorage.setItem("result2", strText3);
-                localStorage.setItem("carType", carTypemsg);
-                localStorage.setItem("tripType", strText8);
-                localStorage.setItem("fare", rate);
-                window.location.href = "bookingSuccess.html";
-            }
-        })                
-        }
 
-});
+    // Routing
+    directionsService.route(request, function (result, status) {
+        if (status == google.maps.DirectionsStatus.OK) {
+
+            //Get distance and time            
+            var d=result.routes[0].legs[0].distance.value/1000;
+            n = d.toFixed(0);
+            sedano=((n*13)+400);
+            sedanr=2*((n*10)+400);
+            psedano=((n*14)+400);
+            psedanr=2*((n*11)+400);
+            suvo=((n*18)+400);
+            suvr=2*((n*13)+400);
+            psuvo=((n*19)+400);
+            psuvr=2*((n*14)+400);
+            traveller=(2*((n*18)+500));
+            
+            
+            //    var nElement = document.createElement("input");
+            //    nElement.id = "nElement";
+            //    nElement.style.display = "none";
+            //    nElement.type = "text";
+            //    nElement.value = `${n}`;
+            //    console.log(nElement,nElement.value);
+            console.log(n);
+               var strText8 = document.getElementById("ser").value;
+               var returndate = document.getElementById("returndate");
+               // strText8="One-Way";
+               var oneway = document.getElementById("oneway");
+            //    oneway.appendChild(nElement);
+                var twoway = document.getElementById("twoway");
+            if(strText8=="One-Way"){
+                twoway.style.display = "none";
+                oneway.style.display = "block";
+                $(".Dist").html(n);
+                $("#1").html(psedano);
+                $("#3").html(psuvo);    
+                $("#5").html(sedano);     
+                $("#7").html(suvo);    
+                returndate.style.display = "none";
+            }
+            else{
+                twoway.style.display = "block";
+                oneway.style.display = "none";
+                n=n*2;
+                $(".Dist").html(n);
+                $("#2").html(psedanr);
+                $("#4").html(psuvr);
+                $("#6").html(suvr);
+                $("#8").html(sedanr);
+                $("#9").html(traveller);
+                returndate.style.display = "block";
+                returndate.attributes.required = "required"; 
+            }
+        
+           
+ /*         //  $("#output").html("<div class='result-table'> Driving distance: " + result.routes[0].legs[0].distance.text + ".<br />SUV Rate: ₹" +((n)*12)+"-"+((n)*15)+".<br />Sedan Rate: ₹" +((n)*8)+"-"+((n)*11)+".<br />Hatchback Rate: ₹" +((n)*16)+"-"+((n)*19)+ ".<br />Duration: " + result.routes[0].legs[0].duration.text + ".</div>");
+            //document.getElementById("output").style.display = "block";
+           // $("#output1").html("<div class='meta-item;'>  <span> Distance: " + result.routes[0].legs[0].distance.text + "</span><br /><span> One Way : ₹" +sedano+"</span><br /><span> Round Way : ₹" +sedanr+"</span></div>");
+            document.getElementById("output1").style.display = "block";
+            $("#output2").html("<div class='meta-item;'>  <span> Distance: " + result.routes[0].legs[0].distance.text + "</span><br /><span> One Way : ₹" +suvo+"</span><br /><span> Round Way : ₹" +suvr+"</span></div>");
+            document.getElementById("output1").style.display = "block";
+            $("#output3").html("<div class='meta-item;'>  <span> Distance: " + result.routes[0].legs[0].distance.text + "</span><br /><span> Round Way : ₹" +traveller+"</span><br /><span> One Way : Not available </span></div>");
+            document.getElementById("output1").style.display = "block";
+            //display route <span> Price  :  ₹13 / Km.</span>
+            directionsDisplay.setDirections(result);
+            
+*/
+
+
+        } else {
+            //delete route from map
+            directionsDisplay.setDirections({ routes: [] });
+            //center map in London
+            map.setCenter(myLatLng);
+
+            //Show error message           
+           
+            alert("Can't find road! Please try again!");
+            clearRoute();
+        }
+    });
+
 }
+
+// Clear results
+
+function clearRoute(){
+    document.getElementById("output").style.display = "none";
+    document.getElementById("location-1").value = "";
+    document.getElementById("location-2").value = "";
+    directionsDisplay.setDirections({ routes: [] });
+    
+}
+
+// Create autocomplete objects for all inputs
+
+var options = {
+    types: ['(india)']
+}
+
+
+var input1 = document.getElementById("location-1");
+var autocomplete1 = new google.maps.places.Autocomplete(input1);
+
+var input2 = document.getElementById("location-2");
+var autocomplete2 = new google.maps.places.Autocomplete(input2);
