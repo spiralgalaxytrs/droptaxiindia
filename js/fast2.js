b7da011d8805d1666b6eb95e2e0b2f52bb21041e
spiralgalaxytrs <80592602+spiralgalaxytrs@users.noreply.github.com>
2021-07-09 23:03:07 +0530

    Update fast2.js

--- js/fast2.js
+++ js/fast2.js
@@ -48,7 +48,7 @@ function book() {
         console.log("result set in one way");
         result = 'Customer Name:  ' + strText + '%0APhone Number: ' + strText1 +'%0APickup Location: ' + strText2+  '%0ADrop Location: ' + strText3 + '%0APickup Date: ' + strText4 + '%0APickup Time: ' + strText6 +'%0AService: ' + strText8 + '%0ACars: ' + carTypemsg+"%0APrice:"+rate+"%0ATotalKms: "+n;
     }else{
-        result = 'Customer Name:  ' + strText + '%0APhone Number: ' + strText1 +'%0APickup Location: ' + strText2+  '%0ADrop Location: ' + strText3 + '%0APickup Date: ' + strText4 +'%0ADrop Date:'+ strText9 + '%0APickup Time: ' + strText6 +'%0AService: ' + strText8 + '%0ACars: ' + strText7+"%0APrice:"+rate+"%0ATotalKms: "+n*2;
+        result = 'Customer Name:  ' + strText + '%0APhone Number: ' + strText1 +'%0APickup Location: ' + strText2+  '%0ADrop Location: ' + strText3 + '%0APickup Date: ' + strText4 +'%0ADrop Date:'+ strText9 + '%0APickup Time: ' + strText6 +'%0AService: ' + strText8 + '%0ACars: ' + strText7+"%0APrice:"+rate+"%0ATotalKms: "+n;
     }
     console.log(result);
     document.getElementById("bookingForm").addEventListener("submit", (e) => {
@@ -69,7 +69,6 @@ if(strText==""||strText1==""||strText2==""||strText3==""||strText4==""||strText6
             if(response.status==200)
             {
                 console.log("Sent Message successfully");
-                
                 localStorage.setItem("result", strText);
                 localStorage.setItem("result1", strText2);
                 localStorage.setItem("result2", strText3);
